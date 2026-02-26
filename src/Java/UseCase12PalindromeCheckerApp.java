d83f8b91ba44ad24662e0aebff7f4dbf5dd13f68
Saumya Bisht <sambisht123k@gmail.com>
2026-02-26 23:27:10 +0530

    Add UC12 strategy pattern implementation for palindrome algorithms

---
+++ src/Java/UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,88 @@
+package Java;
+
+import java.util.*;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+// Stack-based strategy
+class StackStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+
+        for (char ch : input.toCharArray()) {
+            if (ch != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Deque-based strategy
+class DequeStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char ch : input.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Service class using strategy
+class PalindromeService {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String input) {
+        return strategy.check(input);
+    }
+}
+
+// Application entry
+public class UseCase12PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Choose strategy dynamically
+        PalindromeStrategy strategy = new DequeStrategy();
+        // You could switch to:
+        // PalindromeStrategy strategy = new StackStrategy();
+
+        PalindromeService service = new PalindromeService(strategy);
+
+        if (service.checkPalindrome(input)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+    }
+}
